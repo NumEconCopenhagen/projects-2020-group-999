9d42a96a35fd1d8bead722dc329e6d4335c2935d
soraemon123 <53983296+soraemon123@users.noreply.github.com>
2020-03-16 00:46:20 +0100

    git:sync

--- inauguralproject/inauguralproject.py
+++ inauguralproject/inauguralproject.py
@@ -1,4 +1,25 @@
-def square(x):
+import numpy as np 
+from math import log 
+# question 1 
+def function(c ,l, nu, e):
+    return log(c)- nu *(l**(1+1/e))/(1+1/e)
+
+def solve(function):   
+    ## constaints and bounds 
+    c = m + wl - (tau0 * w * l + tau1 * max(w*l - kappa, 0))
+    ## parameters 
+    m = 1
+    nu = 10
+    e = 0.3
+    tau0 = 0.4
+    tau1 = 0.1
+    kappa = 0.4
+    return c
+
+
+
+
+def square(x): 
     """ square numpy array
     
     Args:
