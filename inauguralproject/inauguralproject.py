59d41c218e11b4fa21d82f6df52605140774ec91
soraemon123 <53983296+soraemon123@users.noreply.github.com>
2020-03-16 14:17:29 +0100

    siyuan

--- inauguralproject/inauguralproject.py
+++ inauguralproject/inauguralproject.py
@@ -69,19 +69,25 @@ def new_tax_rev(w,m=1,v=10,e=0.1,t_0=0.4,t_1=0.1,k=0.4):
     return T
 print ("tax revenue" + " " + str(tax_rev(i)))
 
-def opt_tax_rev(x,w,v=10,e=0.4,m=1):
+def opt_tax_rev(x,w,m=1,v=10,e=0.3):
     t_0 = x[0]
     t_1 = x[1]
     k = x[2]
-    return -tax_rev(i)
+    T = 0
+    for i in ws:
+        opt_value = opt(i,m,v,e,t_0,t_1,k)
+        l = opt_value[1]
+        T += t_0*i*1_0 + t_1*max(i*1_0-k,0)
+    return T
 
-initial_guess=[0.4,0.1,0.4]
-sol = optimize.minimize(opt_tax_rev,initial_guess,args=(i,v,e,m),method='Nelder-Mead')
+initial_guess=[0,0,0]
+bounds = ((0,1),(0,1),(0,1))
+sol = optimize.minimize(opt_tax_rev,initial_guess,method='SLSQP',bounds = bounds, args=(i,m,v,e))
 
 t0_opt = sol.x[0]
 t1_opt = sol.x[1]
 k_opt = sol.x[2]
 
-print ("optimal t_0"+ str(t0_opt))
-print ("optimal t_1"+ str(t1_opt))
-print ("optimal k" + str(k))
+print ("optimal t_0"+ " " + str(t0_opt))
+print ("optimal t_1"+ " " + str(t1_opt))
+print ("optimal k" + " " + str(k))
